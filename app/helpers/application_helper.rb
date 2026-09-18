3ccc5e01716502d15e4767a12cff6662fdeda4fc
Dan Daly <Dan Daly>
2026-09-18 17:28:59 +0100

    Updated date_field_to_attribute

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@
 
 module ApplicationHelper
   def date_field_to_attribute(key)
-    key.sub(/\(3i\)$/, '_day')
-      .sub(/\(2i\)$/, '_month')
-      .sub(/\(1i\)$/, '_year')
+    key.sub(/\(3i\)$/, "_day")
+      .sub(/\(2i\)$/, "_month")
+      .sub(/\(1i\)$/, "_year")
   end
 end
