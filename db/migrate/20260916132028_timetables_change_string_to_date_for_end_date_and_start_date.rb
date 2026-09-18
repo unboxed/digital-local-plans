4d22a7571d9535291e898a23289fdd95217db6e5
Dan Daly <Dan Daly>
2026-09-18 10:54:09 +0100

    Make change_column migration reversible

--- db/migrate/20260916132028_timetables_change_string_to_date_for_end_date_and_start_date.rb
+++ db/migrate/20260916132028_timetables_change_string_to_date_for_end_date_and_start_date.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 class TimetablesChangeStringToDateForEndDateAndStartDate < ActiveRecord::Migration[8.1]
-  def change
-    change_column :timetables, :period_end_date, :date, 
-                  using: 'period_end_date::date'
-                    
-    change_column :timetables, :period_start_date, :date, 
-                  using: 'period_start_date::date'
+  def up
+    change_column :timetables, :period_end_date, :date
+    change_column :timetables, :period_start_date, :date
+  end
+
+  def down
+    change_column :timetables, :period_end_date, :string
+    change_column :timetables, :period_start_date, :string
   end
 end
